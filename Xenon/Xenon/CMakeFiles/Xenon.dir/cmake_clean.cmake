d2714f95dcebed19282de0430d4c58cff318fca1
Jeremy Ferguson <jmfergie@berkeley.edu>
2020-07-18 20:19:32 -0500

    Changed macros to try and fix bug

---
+++ Xenon/Xenon/CMakeFiles/Xenon.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Xenon.dir/Xenon.cc.o"
+  "CMakeFiles/Xenon.dir/src/DegradExcitationHit.cc.o"
+  "CMakeFiles/Xenon.dir/src/DegradModel.cc.o"
+  "CMakeFiles/Xenon.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/Xenon.dir/src/DetectorMessenger.cc.o"
+  "CMakeFiles/Xenon.dir/src/DriftLineTrajectory.cc.o"
+  "CMakeFiles/Xenon.dir/src/DriftLineTrajectoryPoint.cc.o"
+  "CMakeFiles/Xenon.dir/src/EventAction.cc.o"
+  "CMakeFiles/Xenon.dir/src/GarfieldExcitationHit.cc.o"
+  "CMakeFiles/Xenon.dir/src/GarfieldVUVPhotonModel.cc.o"
+  "CMakeFiles/Xenon.dir/src/GasBoxSD.cc.o"
+  "CMakeFiles/Xenon.dir/src/GasModelParameters.cc.o"
+  "CMakeFiles/Xenon.dir/src/GasModelParametersMessenger.cc.o"
+  "CMakeFiles/Xenon.dir/src/MyUserActionInitialization.cc.o"
+  "CMakeFiles/Xenon.dir/src/PhysListEmStandard.cc.o"
+  "CMakeFiles/Xenon.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/Xenon.dir/src/PhysicsListMessenger.cc.o"
+  "CMakeFiles/Xenon.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/Xenon.dir/src/RunAction.cc.o"
+  "CMakeFiles/Xenon.dir/src/SteppingAction.cc.o"
+  "CMakeFiles/Xenon.dir/src/XenonHit.cc.o"
+  "Xenon.pdb"
+  "Xenon"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Xenon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
